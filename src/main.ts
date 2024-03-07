e1295bc847927afb4c7da61386e19e52fcbc05e0
siwonpada <psw041115@gmail.com>
2024-03-08 03:50:42 +0900

    attach: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Vapor-auth API')
+    .setDescription('The Vapor-auth API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // start server
   await app.listen(3000);
 }
 bootstrap();
