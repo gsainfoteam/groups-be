312ba19f5c19c7ce7a8953a7fa12fee431679674
siwonpada <psw041115@gmail.com>
2024-03-08 03:59:49 +0900

    add health-check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,34 @@
 import { Controller, Get } from '@nestjs/common';
+import {
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('health-check')
 export class AppController {
+  @ApiOperation({
+    summary: 'Health check',
+    description: 'Check if the server is up and running',
+  })
+  @ApiOkResponse({
+    description: 'Server is up and running',
+    content: {
+      'application/json': {
+        examples: {
+          pong: {
+            value: 'pong',
+            description: 'Server is up and running',
+          },
+        },
+      },
+    },
+  })
+  @ApiInternalServerErrorResponse({ description: 'Internal server error' })
   @Get()
-  getHello(): string {
+  async health(): Promise<string> {
     return 'pong';
   }
 }
