0517726ebc4e4e4ac3c4fe76c820b5e7de512f55
siwonpada <psw041115@gmail.com>
2024-03-10 03:01:54 +0900

    modify grammar error

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
 import { GroupModule } from './group/group.module';
-import { MemberModule } from './member/member.module';
 import { UserModule } from './user/user.module';
 import { RoleModule } from './role/role.module';
 
@@ -12,7 +11,6 @@ import { RoleModule } from './role/role.module';
       envFilePath: '.env',
     }),
     GroupModule,
-    MemberModule,
     UserModule,
     RoleModule,
   ],
