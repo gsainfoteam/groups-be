a1a484bfb5bc9da2312727ebc030fb7bd7094a83
siwonpada <psw041115@gmail.com>
2024-03-09 14:32:09 +0900

    feat: user guard

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,12 @@ import { UserService } from './user.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { UserRepository } from './user.repository';
 import { IdpModule } from 'src/idp/idp.module';
+import { UserStrategy } from './guard/user.strategy';
+import { UserGuard } from './guard/user.guard';
 
 @Module({
   imports: [PrismaModule, IdpModule],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UserStrategy, UserGuard],
+  exports: [UserService, UserGuard],
 })
 export class UserModule {}
