a8da61832791dd638a940ed9e9b962bc451aa820
rabo8497 <jeon9799@gmail.com>
2024-03-25 04:06:41 +0900

    add test code

--- test/group/group.repository.spec.ts
+++ test/group/group.repository.spec.ts
@@ -3,7 +3,46 @@ import { GroupRepository } from '../../src/group/group.repository';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { testConfigModule } from 'test/config/testConfig.module';
+import { CreateGroupDto } from 'src/group/dto/req/createGroup.dto';
 
+/*
+// create group
+describe('create group', () => {
+  let repository: GroupRepository;
+  let prismaService: PrismaService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule, testConfigModule],
+      providers: [
+        GroupRepository,
+      ],
+    }).compile();
+
+    repository = module.get<GroupRepository>(GroupRepository);
+    prismaService = module.get<PrismaService>(PrismaService);
+  });
+
+  it('should be defined', () => {
+    expect(repository).toBeDefined();
+  });
+
+  describe('createGroup', () => {
+    it("should create a new group", async () => {
+      const groupDto: CreateGroupDto = {
+        name: "TestGroup2",
+        description: "This is a test group",
+      };
+
+      const result = await repository.createGroup(groupDto);
+      expect(result).toHaveProperty('name');
+      expect(result.name).toBe(groupDto.name);  
+      expect(result.description).toBe(groupDto.description);
+
+    });
+  });
+});
+*/
 
 // create user role
 describe('create user role', () => {
