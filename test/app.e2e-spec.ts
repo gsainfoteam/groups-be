ad0fb8c6aadc7565c80346fb754c96e9f5fb318d
rabo8497 <jeon9799@gmail.com>
2024-05-08 20:28:05 +0900

    update test code

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -11,15 +11,15 @@ describe('GroupController (e2e)', () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     })
-    .overrideGuard(UserGuard)
-    .useValue({
-      canActivate: overrideGuardFunction ? overrideGuardFunction : (context: ExecutionContext) => {
-        const req = context.switchToHttp().getRequest();
-        req.user = { uuid: 'd75f9a3b-b110-437e-9287-eaaf5965849d' }; // TestUser
-        return true;
-      }
-    })
-    .compile();
+      .overrideGuard(UserGuard)
+      .useValue({
+        canActivate: overrideGuardFunction ? overrideGuardFunction : (context: ExecutionContext) => {
+          const req = context.switchToHttp().getRequest();
+          req.user = { uuid: 'd75f9a3b-b110-437e-9287-eaaf5965849d' }; // TestUser
+          return true;
+        }
+      })
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
@@ -79,27 +79,27 @@ describe('GroupController (e2e)', () => {
     });
 
     it('올바르게 Group을 반환하는가.', async () => {
-        app = await initializeApp();
-        const groupName = 'NewTestTeam'; 
-        await request(app.getHttpServer())
-          .get(`/group/${groupName}`)
-          .expect(200)
-          .then(response => {
-            expect(response.body).toHaveProperty('name', groupName);
-            expect(response.body).toHaveProperty('description');
-          });
-      });
-  
-      it('없는 Group을 Get요청할 때 처리할 수 있는가.', async () => {
-        app = await initializeApp();
-        const groupName = 'NonExistentGroup';
-        await request(app.getHttpServer())
-          .get(`/group/${groupName}`)
-          .expect(404)
-          .then(response => {
-            expect(response.body.message).toContain('does not exist');
-          });
-      });
+      app = await initializeApp();
+      const groupName = 'NewTestTeam';
+      await request(app.getHttpServer())
+        .get(`/group/${groupName}`)
+        .expect(200)
+        .then(response => {
+          expect(response.body).toHaveProperty('name', groupName);
+          expect(response.body).toHaveProperty('description');
+        });
+    });
+
+    it('없는 Group을 Get요청할 때 처리할 수 있는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'NonExistentGroup';
+      await request(app.getHttpServer())
+        .get(`/group/${groupName}`)
+        .expect(404)
+        .then(response => {
+          expect(response.body.message).toContain('does not exist');
+        });
+    });
   });
 
   describe('/group/:name (PATCH)', () => {
@@ -159,6 +159,178 @@ describe('GroupController (e2e)', () => {
         .then(response => {
           expect(response.body.message).toContain('does not exist');
         });
+    });
+  });
+
+  describe('/group/:name/member (POST)', () => {
+    it('group에 member을 올바르게 추가할 수 있는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+      const newMember = {
+        uuid: '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd' // 새로운 user
+      };
+
+      await request(app.getHttpServer())
+        .post(`/group/${groupName}/member`)
+        .send(newMember)
+        .expect(201)
+        .then(response => {
+          expect(response.body).toHaveProperty('groupName', groupName);
+          expect(response.body).toHaveProperty('userUuid', newMember.uuid);
+          expect(response.body).toHaveProperty('createdAt');
+        });
+    });
+
+    it('member가 이미 group에 있을 때 처리할 수 있는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+      const existingMember = {
+        uuid: '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd'
+      };
+
+      await request(app.getHttpServer())
+        .post(`/group/${groupName}/member`)
+        .send(existingMember)
+        .expect(409)
+        .then(response => {
+          expect(response.body.message).toContain('User already exists in group');
+        });
+    });
+  });
+
+  describe('/group/:name/member (GET)', () => {
+    it('성공적으로 group member 리스트를 받아오는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+
+      await request(app.getHttpServer())
+        .get(`/group/${groupName}/member`)
+        .expect(200)
+        .then(response => {
+          expect(Array.isArray(response.body)).toBeTruthy();
+          expect(response.body.length).toBeGreaterThan(0);
+          expect(response.body[0]).toHaveProperty('uuid');
+        });
+    });
+  });
+
+  describe('/group/:name/member/:uuid (DELETE)', () => {
+    it('group에 추가했던 user을 성공적으로 제거할 수 있는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+      const memberUuid = '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd';
+
+      await request(app.getHttpServer())
+        .delete(`/group/${groupName}/member/${memberUuid}`)
+        .expect(200)
+        .then(response => {
+          expect(response.body).toHaveProperty('groupName', groupName);
+          expect(response.body).toHaveProperty('userUuid', memberUuid);
+        });
+    });
+
+    it('group에서 존재하지 않는 user을 제거할 때 처리 가능한가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+      const nonExistentMemberUuid = '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd';
+
+      await request(app.getHttpServer())
+        .delete(`/group/${groupName}/member/${nonExistentMemberUuid}`)
+        .expect(403)
+        .then(response => {
+          expect(response.body.message).toContain("doesn't have permission");
+        });
+    });
+
+    describe('/group/:groupName/role (GET)', () => {
+      it('group의 role들을 올바르게 가져올 수 있는가.', async () => {
+        app = await initializeApp();
+        const groupName = 'TestTeam';
+
+        await request(app.getHttpServer())
+          .get(`/group/${groupName}/role`)
+          .expect(200)
+          .then(response => {
+            expect(Array.isArray(response.body.list)).toBeTruthy();
+            expect(response.body.list.length).toBeGreaterThan(0);
+            expect(response.body.list[0]).toHaveProperty('id');
+            expect(response.body.list[0]).toHaveProperty('name');
+            expect(response.body.list[0]).toHaveProperty('authoities');
+          });
+      });
+    });
+
+    describe('/group/:groupName/role (POST)', () => {
+      it('successfully creates a new role within a group', async () => {
+        app = await initializeApp();
+        const groupName = 'TestTeam';
+        const newRole = {
+          name: 'NewRole',
+          authorities: ['ROLE_CREATE'],
+          externalAuthorities: ['ZIGGLE_WRITE_NOTICE']
+        };
+
+        await request(app.getHttpServer())
+          .post(`/group/${groupName}/role`)
+          .send(newRole)
+          .expect(201)
+        // role 부분의 출력이 없기에 그냥 오류 없는 것만 감지
+      });
+    });
+  });
+
+  describe('/group/:groupName/role/:id (PUT)', () => {
+    it('role이 정상적으로 update될 수 있는가.', async () => {
+      const groupName = 'TestTeam';
+      const roleId = 2;
+      const updateRoleDto = {
+        authorities: ['ROLE_UPDATE'],
+        externalAuthorities: ['ZIGGLE_MANAGE_TASKS']
+      };
+
+      await request(app.getHttpServer())
+        .put(`/group/${groupName}/role/${roleId}`)
+        .send(updateRoleDto)
+        .expect(200)
+    });
+  });
+
+  describe('/group/:groupName/member/:uuid/role/:id (POST)', () => {
+    it('user에 role을 적용할 수 있는가.', async () => {
+      const groupName = 'TestTeam';
+      const roleId = 2; // 존재하는 역할 ID
+      const newMember = {
+        uuid: '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd' // 새로운 user
+      };
+
+      await request(app.getHttpServer())
+        .post(`/group/${groupName}/member/${newMember.uuid}/role/${roleId}`)
+        .expect(201)
+    });
+  });
+
+  describe('/group/:groupName/member/:uuid/role/:id (DELETE)', () => {
+    it('user에게 적용된 rule을 지울 수 있는가.', async () => {
+      const groupName = 'TestTeam';
+      const memberUuid = '9cb6a0d8-3c07-4311-a5eb-3e1dd6628bbd';
+      const roleId = 2;
+
+      await request(app.getHttpServer())
+        .delete(`/group/${groupName}/member/${memberUuid}/role/${roleId}`)
+        .expect(200)
+    });
+  });
+
+  describe('/group/:groupName/role/:id (DELETE)', () => {
+    it('group의 role을 성공적으로 삭제할 수 있는가.', async () => {
+      app = await initializeApp();
+      const groupName = 'TestTeam';
+      const roleId = 2;
+
+      await request(app.getHttpServer())
+        .delete(`/group/${groupName}/role/${roleId}`)
+        .expect(200)
+      // role 부분의 출력이 없기에 그냥 오류 없는 것만 감지
     });
   });
 
