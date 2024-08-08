48ac97237f280c6f132e5e36eae0652202f7f520
siwonpada <psw041115@gmail.com>
2024-08-08 21:52:34 +0900

    update: use logger

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { IdpService } from 'src/idp/idp.service';
 import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
+  private readonly logger = new Logger(AuthService.name);
   constructor(
     private readonly userService: UserService,
     private readonly idpService: IdpService,
   ) {}
 
   async login(accessToken: string): Promise<{ accessToken: string }> {
+    this.logger.log('Login with IDP');
     const { uuid, name, email } =
       await this.idpService.getUserInfo(accessToken);
     await this.userService.upsertUser({ uuid, name, email });
@@ -18,6 +20,7 @@ export class AuthService {
   }
 
   async validateUser(accessToken: string): Promise<User> {
+    this.logger.log('Validate user');
     const { uuid } = await this.idpService.getUserInfo(accessToken);
     return this.userService.getUserInfo(uuid);
   }
