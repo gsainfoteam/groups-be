5eaa7f8d8643f5b993bcbe67b1cf8e0daf8842f0
Shiwon Park <psw041115@gmail.com>
2024-08-09 02:44:33 +0900

    update: redis module config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { IdpModule } from './idp/idp.module';
 import { HealthModule } from './health/health.module';
 import { ClientModule } from './client/client.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { GroupModule } from './group/group.module';
-import { RedisModule } from '@nestjs-modules/ioredis';
 import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-    }),
-    RedisModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'single',
-        url: configService.getOrThrow<string>('REDIS_URL'),
-      }),
     }),
     IdpModule,
     HealthModule,
--- src/group/group.module.ts
+++ src/group/group.module.ts
@@ -1,14 +1,27 @@
 import { Module } from '@nestjs/common';
-import { GroupController } from './group.controller';
 import { GroupService } from './group.service';
-import { PrismaModule } from 'src/prisma/prisma.module';
-import { AuthModule } from 'src/auth/auth.module';
-import { RedisModule } from '@nestjs-modules/ioredis';
+import { GroupController } from './group.controller';
 import { GroupRepository } from './group.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserModule } from 'src/user/user.module';
+import { RedisModule } from '@nestjs-modules/ioredis';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, AuthModule, RedisModule],
-  controllers: [GroupController],
+  imports: [
+    ConfigModule,
+    PrismaModule,
+    UserModule,
+    RedisModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'single',
+        url: configService.getOrThrow<string>('REDIS_URL'),
+      }),
+    }),
+  ],
   providers: [GroupService, GroupRepository],
+  controllers: [GroupController],
 })
 export class GroupModule {}
