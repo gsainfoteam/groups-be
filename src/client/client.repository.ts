2a25f84f5ad0b16c5e0d36b7130226cf1ddcff88
ysym1208 <soyul.yoon@gm.gist.ac.kr>
2024-11-06 18:14:11 +0900

    Client가 등록한 authorities 확인 API 추가

--- src/client/client.repository.ts
+++ src/client/client.repository.ts
@@ -157,34 +157,21 @@ export class ClientRepository {
    * @param uuid UUID of the client
    * @returns Client object with authorities
    */
-  async getClientWithAuthorities(
-    uuid: string,
-  ): Promise<Client & { ExternalAuthority: { authority: string }[] }> {
+  async getClientWithAuthorities(uuid: string): Promise<Client & { ExternalAuthority: { authority: string }[] }> {
     this.logger.log(`Retrieving client with authorities for uuid: ${uuid}`);
-    return this.prismaService.client
-      .findUnique({
-        where: { uuid },
-        include: {
-          ExternalAuthority: {
-            select: { authority: true },
-          },
-        },
-      })
-      .then((client) => {
-        if (!client) {
-          this.logger.debug(`client not found`);
-          throw new ForbiddenException('client not found');
-        }
-        return client;
-      })
-      .catch((error) => {
-        if (error instanceof PrismaClientKnownRequestError) {
-          this.logger.error(`unknown database error`);
-          throw new InternalServerErrorException('unknown database error');
-        }
-        this.logger.error(`unknown error`);
-        throw new InternalServerErrorException('unknown error');
-      });
-    
+  
+    const client = await this.prismaService.client.findUnique({
+      where: { uuid },
+      include: {
+        ExternalAuthority: true,
+      },
+    });
+  
+    if (!client) {
+      this.logger.debug(`client not found`);
+      throw new ForbiddenException('client not found');
+    }
+  
+    return client;
   }
 }
