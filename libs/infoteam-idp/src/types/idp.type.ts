e8bfd98631796c17a9ba9e94397ced4594680331
siwonpada <psw041115@gmail.com>
2025-06-29 17:29:23 +0900

    IdP의 client credential 사용 Fixes #140

--- libs/infoteam-idp/src/types/idp.type.ts
+++ libs/infoteam-idp/src/types/idp.type.ts
@@ -1,12 +1,38 @@
-/** jwt api response type from infoteam idp */
-export type IdpJwtResponse = {
-  access_token: string;
-  refresh_token: string;
-};
-
 /** user info api response type from infoteam idp */
 export type IdpUserInfoRes = {
   sub: string;
-  email: string;
+  profile: string;
+  picture: string;
   name: string;
+  email: string;
+  student_id: string;
+};
+
+/** id token payload */
+export type IdTokenPayload = {
+  sub: string;
+  aud: string;
+  scope: string;
+  nonce: string;
+  profile: string;
+  picture: string;
+  name: string;
+  email: string;
+  student_id: string;
+};
+
+/** Client credential api request type to infoteam idp */
+export type ClientAccessTokenRequest = {
+  grant_type: 'client_credentials';
+  client_id: string;
+  client_secret: string;
+  scope?: string;
+};
+
+/** Client credential api response type from infoteam idp */
+export type ClientAccessTokenResponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
 };
